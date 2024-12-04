42d01fb30789ba1a50250fe4e862d6a9c4984168
Confetto22 <enockagyei1999@gmail.com>
2024-12-04 13:46:01 +0000

    footer background

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -90,93 +90,95 @@ const resources = footerLinks.filter(
 
 const Footer = () => {
   return (
-    <footer className="bg-[#1a1411] p-4 pt-12 flex flex-col items-center ">
-      <div className="footer_columns grid grid-cols-1 md:grid-cols-3 gap-3 lg:gap-24 max-w-[600px]  md:max-w-full">
-        <div className="max-w-[300px] flex flex-col gap-3">
-          <img
-            src="https://res.cloudinary.com/dv9aqxptd/image/upload/v1717265448/homchapel/homlogo_ucmyp7.webp"
-            alt="church logo"
-            className="footer_logo w-[200px] mb-4"
-          />
+    <footer className="bg-[url('https://res.cloudinary.com/dv9aqxptd/image/upload/v1733016490/homchapel/DSC_4237_bhpc9u.webp')]   bg-cover bg-center min-h-[70vh]">
+      <div className="footer_cover bg-[#000000cd] p-4 pt-12 flex flex-col items-center justify-center min-h-[70vh]">
+        <div className="footer_columns grid grid-cols-1 md:grid-cols-3 gap-3 lg:gap-24 max-w-[600px]  md:max-w-full">
+          <div className="max-w-[300px] flex flex-col gap-3">
+            <img
+              src="https://res.cloudinary.com/dv9aqxptd/image/upload/v1717265448/homchapel/homlogo_ucmyp7.webp"
+              alt="church logo"
+              className="footer_logo w-[200px] mb-4"
+            />
 
-          <p className="text-sm text-[#a3a2a2]">
-            Thank you for visiting us online. We invite you to join us in
-            worship, fellowship, and service as we grow together in faith &
-            love. God bless you!
-          </p>
-          <div className="text-[#a3a2a2] text-[1rem] flex flex-col gap-1">
-            <Link
-              to={"#"}
-              className="hover:text-[var(--second-color)] ease-in-out duration-200 hover:underline underline-offset-2"
-            >
-              Tatop Weija, Accra
-            </Link>
-            <p>
-              Phone :{" "}
+            <p className="text-sm text-[#a3a2a2]">
+              Thank you for visiting us online. We invite you to join us in
+              worship, fellowship, and service as we grow together in faith &
+              love. God bless you!
+            </p>
+            <div className="text-[#a3a2a2] text-[1rem] flex flex-col gap-1">
               <Link
                 to={"#"}
                 className="hover:text-[var(--second-color)] ease-in-out duration-200 hover:underline underline-offset-2"
               >
-                0593552731
+                Tatop Weija, Accra
               </Link>
-            </p>
+              <p>
+                Phone :{" "}
+                <Link
+                  to={"#"}
+                  className="hover:text-[var(--second-color)] ease-in-out duration-200 hover:underline underline-offset-2"
+                >
+                  0593552731
+                </Link>
+              </p>
+            </div>
+            <div className="footerSocials flex items-center gap-2 mt-2">
+              {footerSocials.map((social) => (
+                // <Link
+                //   to={"#"}
+                //   key={social.refName}
+                //   className="bg-transparent border border-[#a3a2a2] rounded-full text-white flex items-center justify-center hover:text-[var(--second-color)]  ease-in-out duration-200"
+                //   aria-label={`${social.refName} link`}
+                // >
+                //   <button
+                //     className="w-[40px] h-[40px] flex items-center justify-center "
+                //     aria-label={`${social.refName} link`}
+                //   >
+                //     {social.icon}
+                //   </button>
+                // </Link>
+                <Link
+                  to={social.ref}
+                  target="_blank"
+                  key={social.refName}
+                  className="bg-transparent border border-[#a3a2a2] rounded-full text-white flex items-center justify-center hover:text-[var(--second-color)]  ease-in-out duration-200 w-[40px] h-[40px] flex items-center justify-center"
+                  aria-label={`${social.refName} link`}
+                >
+                  {social.icon}
+                </Link>
+              ))}
+            </div>
           </div>
-          <div className="footerSocials flex items-center gap-2 mt-2">
-            {footerSocials.map((social) => (
-              // <Link
-              //   to={"#"}
-              //   key={social.refName}
-              //   className="bg-transparent border border-[#a3a2a2] rounded-full text-white flex items-center justify-center hover:text-[var(--second-color)]  ease-in-out duration-200"
-              //   aria-label={`${social.refName} link`}
-              // >
-              //   <button
-              //     className="w-[40px] h-[40px] flex items-center justify-center "
-              //     aria-label={`${social.refName} link`}
-              //   >
-              //     {social.icon}
-              //   </button>
-              // </Link>
-              <Link
-                to={social.ref}
-                target="_blank"
-                key={social.refName}
-                className="bg-transparent border border-[#a3a2a2] rounded-full text-white flex items-center justify-center hover:text-[var(--second-color)]  ease-in-out duration-200 w-[40px] h-[40px] flex items-center justify-center"
-                aria-label={`${social.refName} link`}
-              >
-                {social.icon}
-              </Link>
-            ))}
+
+          <div className="allFooterLinks grid gap-6 grid-cols-2 md:grid-cols-2 max-w-[300px]">
+            <ul className="footerLinks">
+              {quickLinks.map((singleQuickLink) => (
+                <li key={singleQuickLink.name}>
+                  <Link to={singleQuickLink.ref}>{singleQuickLink.name}</Link>
+                </li>
+              ))}
+            </ul>
+            <ul className="footerLinks">
+              {resources.map((singleResource) => (
+                <li key={singleResource.name}>
+                  <Link to={singleResource.ref}>{singleResource.name}</Link>
+                </li>
+              ))}
+            </ul>
           </div>
+          <Form />
         </div>
 
-        <div className="allFooterLinks grid gap-6 grid-cols-2 md:grid-cols-2 max-w-[300px]">
-          <ul className="footerLinks">
-            {quickLinks.map((singleQuickLink) => (
-              <li key={singleQuickLink.name}>
-                <Link to={singleQuickLink.ref}>{singleQuickLink.name}</Link>
-              </li>
-            ))}
-          </ul>
-          <ul className="footerLinks">
-            {resources.map((singleResource) => (
-              <li key={singleResource.name}>
-                <Link to={singleResource.ref}>{singleResource.name}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <Form />
+        <p className="copyright text-center mt-5 text-[#a3a2a2] capitalize">
+          developed by{" "}
+          <Link
+            to={"#"}
+            className="text-white underline font-semibold text-[1.1rem]"
+          >
+            Confetto&copy;
+          </Link>
+        </p>
       </div>
-
-      <p className="copyright text-center mt-5 text-[#a3a2a2] capitalize">
-        developed by{" "}
-        <Link
-          to={"#"}
-          className="text-white underline font-semibold text-[1.1rem]"
-        >
-          Confetto&copy;
-        </Link>
-      </p>
     </footer>
   );
 };
